f8a88cef1a2e24ae67bd5597ad7720fe2f13bfb2
Mattyzcreationz <141065370+Mattyzcreationz@users.noreply.github.com>
2024-03-31 20:45:31 -0500

    Add files via upload

---
+++ Quer/queries_playstores.sql
@@ -0,0 +1,16 @@
+--
+--SELECT id FROM analytics = 1880;
+--SELECT app_name, id FROM analytics WHERE last_updated = '2018-08-01';
+--SELECT catergory, COUNT(*) AS count FROM analytics GROUP BY category; 
+-- SELECT appname, COUNT(*) AS review_count FROM analytics GROUP BY app_name ORDER BY review_count DESC LIMIT 5;
+--SELECT app_name, MAX(reviews) AS max_reviews FROM analytics WHERE rating >= 4.8 GROUP BY app_name ORDER BY max_reviews DESC LIMIT 1;
+--SELECT category, AVG(rating) AS avg_rating FROM analytics GROUP BY category ORDER BY avg_rating DESC;
+--SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+--SELECT * FROM analytics WHERE min_installs <= 50 AND rating IS NOT NULL ORDER BY rating DESC;
+--SELECT app_name FROM analytics WHERE rating < 3 AND reviews >= 10000;
+--SELECT app_name,reviews FROM analytics WHERE price  >= 0.10 AND price <= 1.00 ORDER BY reviews DESC LIMIT 10;
+--SELECT app_name, last_updated FROM analytics ORDER BY last_upated ASC LIMIT 1;
+--SELECT app_name, price FROM analytics ORDER BY price DESC LIMIT 1;
+--SELECT SUM(reviews) AS total_reviews FROM analytics;
+--SELECT category, COUNT(*) AS app_count FROM analytics GROUP BY category HAVING COUNT(*) > 300;
+--SELECT app_name, reviews, min_installs, (min_installs::float / reviews) AS proportionFROM analyticsWHERE min_installs >= 100000ORDER BY proportion DESC LIMIT 1;
